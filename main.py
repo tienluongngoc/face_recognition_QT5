fc7f2a4107c9b9ab3909ef89fbeb61114f3b64e2
root <root@rtx2080ti-01.eds.local>
2022-07-14 22:19:12 -0800

    initialize project

---
+++ main.py
@@ -0,0 +1,3 @@
+from utils.logger import Logger
+logger = Logger.__call__().get_logger()
+logger.info("Face recognition, initialized")
---
+++ utils/logger.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import logging
+import os
+import datetime
+import time
+
+
+class SingletonType(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonType, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Logger(object, metaclass=SingletonType):
+    _logger = None
+
+    def __init__(self):
+        self._logger = logging.getLogger("face recognition")
+        self._logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s \t [%(levelname)s | %(filename)s:%(lineno)s] > %(message)s')
+
+        now = datetime.datetime.now()
+        dirname = "./logs"
+
+        if not os.path.isdir(dirname):
+            os.mkdir(dirname)
+        fileHandler = logging.FileHandler(dirname + "/log_" + now.strftime("%Y-%m-%d")+".log")
+
+        streamHandler = logging.StreamHandler()
+
+        fileHandler.setFormatter(formatter)
+        streamHandler.setFormatter(formatter)
+
+        self._logger.addHandler(fileHandler)
+        self._logger.addHandler(streamHandler)
+
+    def get_logger(self):
+        return self._logger
+
+# # a simple usecase 
+# if __name__ == "__main__":
+#     logger = MyLogger.__call__().get_logger()
+#     logger.info("Hello, Logger")
+#     logger.debug("bug occured")
